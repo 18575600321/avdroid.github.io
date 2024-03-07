90f59389f5f0af5b66fd046114228a95d83bb480
Hu Yongbao <18575600321@163.com>
2024-03-07 18:21:49 +0800

    Site updated: 2024-03-07 18:21:48

--- photos/ins.js
+++ photos/ins.js
@@ -79,11 +79,7 @@
       };
     }
 
-    /**
-     * @name impush-client 
-     * @description 这个项目让我发家致富…
-     * @date 2016-12-1
-     */
+
 
     var _collection = [];
     var _count = 0;
@@ -116,15 +112,15 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://avdroid.github.io/min_photos/' + data.link[i];
-          var src = 'https://avdroid.github.io/photos/' + data.link[i];
+          var minSrc = 'http://android.ticp.io:18088/min_photos/' + data.link[i];
+          var src = 'http://android.ticp.io:18088/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
@@ -169,7 +165,7 @@
     function loadData(success) {
       if (!searchData) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', './data.json?t=' + +new Date(), true);
+        xhr.open('GET', '/photos/data.json?t=' + +new Date(), true);
 
         xhr.onload = function() {
           if (this.status >= 200 && this.status < 300) {
